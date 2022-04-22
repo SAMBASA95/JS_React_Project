4a96bbb4e2fbcb3bb6c91626b8ad7ffe4790afca
SAMBASA <83223911+SAMBASA95@users.noreply.github.com>
2022-04-22 12:47:11 +0530

    Create unsplash.js

---
+++ pics/components/api/unsplash.js
@@ -0,0 +1,10 @@
+// dT3kAsBvpltfa2uIn-JgWYAg8voZ0kSM6ROA_hC4RGA
+import axios from 'axios';
+
+export default axios.create({
+    baseURL: 'https://api.unsplash.com/',
+    headers: {
+        Authorization:
+            'Client-ID dT3kAsBvpltfa2uIn-JgWYAg8voZ0kSM6ROA_hC4RGA',
+    },
+});
