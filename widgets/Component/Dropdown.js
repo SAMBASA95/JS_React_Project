7f1e584d6af75d167afc1a8471768d54cc1c28ea
SAMBASA <83223911+SAMBASA95@users.noreply.github.com>
2022-05-20 14:50:19 +0530

    Update Dropdown.js

--- widgets/Component/Dropdown.js
+++ widgets/Component/Dropdown.js
@@ -1,8 +1,30 @@
-import React from 'react'
+import React, { useState, useEffect, useRef } from "react";
 
-const Dropdown = ({ options , selected , onSelectedChange}) => {
+const Dropdown = ({ options, selected, onSelectedChange }) => {
+    const [open, setOpen] = useState(false);
+    const ref = useRef();
+
+    useEffect(() => {
+        const onBodyClick = (event) => {
+            if (ref.current.contains(event.target)) {
+                return;
+            }
+            setOpen(false);
+        };
+        document.body.addEventListener("click", onBodyClick, { capture: true });
+
+        return () => {
+            document.body.removeEventListener("click", onBodyClick, {
+                capture: true,
+            });
+        };
+    }, []);
+
     const renderedOptions = options.map((option) => {
-        // console.log(option.value) 
+        if (option.value === selected.value) {
+            return null;
+        }
+
         return (
             <div
                 key={option.value}
@@ -13,19 +35,25 @@ const Dropdown = ({ options , selected , onSelectedChange}) => {
             </div>
         );
     });
-
+    // console.log(selected);
+    // console.log(ref.current);
     return (
-        <div className="ui form">
+        <div ref={ref} className="ui form">
             <div className="field">
                 <label className="label">Select a Color</label>
-                <div className="ui selection dropdown visible active">
+                <div
+                    onClick={() => setOpen(!open)}
+                    className={`ui selection dropdown ${open ? "visible active" : ""}`}
+                >
                     <i className="dropdown icon"></i>
                     <div className="text">{selected.label}</div>
-                    <div className="menu visible transition">{renderedOptions}</div>
+                    <div className={`menu ${open ? "visible transition" : ""}`}>
+                        {renderedOptions}
+                    </div>
                 </div>
             </div>
         </div>
     );
 };
 
-export default Dropdown
+export default Dropdown;
